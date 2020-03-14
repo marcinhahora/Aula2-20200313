817c18adc0d47d191cbc2589b553bd49c9287ae4
Internet <Internet@LAB206-0505>
2020-03-13 21:50:24 -0300

    teste commit

--- src/aula2/Aula2.java
+++ src/aula2/Aula2.java
@@ -17,6 +17,13 @@ public class Aula2 {
     //função main pode receber um vetor de strings
     public static void main(String[] args) {
         // TODO code application logic here
+        int qtdDiv = 0;
+        int numero = 7;
+        int inicio = numero;
+        
+        while (inicio >= 1)
+        if (numero%inicio==0)
+            qtdDiv++;
     }
     
 }
